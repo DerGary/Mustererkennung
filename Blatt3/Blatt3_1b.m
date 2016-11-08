0e7bc4f0024bd2d1fb9b1c43cb2b7ab10f2c63dc
DerGary <stefangerasch1991@gmail.com>
2016-11-08 09:56:59 +0100

    Fixed 1b Renamed variables of Excercise 2

--- Blatt3/Blatt3_1b.m
+++ Blatt3/Blatt3_1b.m
@@ -3,29 +3,33 @@ close all;
 %Define number of samples to take
 Fs = 128;               % Sampling frequency
 T = 1/Fs;               % Sampling period
-L = 128;                % Length of signal
+L = 4*Fs;                % Length of signal
 %Define signal
 t = (0:L-1)*T;          % Time vector
 signal = f1(t);
 %signal = f2(t);
 
 %Plot to illustrate that it is a sine wave
+subplot(2,2,1);
 plot(t, signal);
 title('Time-Domain signal');
 
 %Take fourier transform
 fftSignal = fft(signal);
-plot(fftSignal);
+% subplot(2,2,1);
+% plot(fftSignal);
+% title('FFT Output');
 
 %apply fftshift, explanation: https://de.mathworks.com/matlabcentral/newsreader/view_thread/285244
 fftSignalShifted = fftshift(fftSignal);
 
 %Next, calculate the frequency axis, which is defined by the sampling rate
-f = Fs/2*linspace(-1,1,Fs);
+f = L/2*linspace(-1,1,L);
 
 %Since the signal is complex, we need to plot the magnitude to get it to
 %look right, so we use abs (absolute value)
 P2_Shifted = abs(fftSignalShifted/L);
+subplot(2,2,2);
 plot(f, P2_Shifted);
 title('Double-Sided Amplitude Spectrum of X(t)');
 xlabel('Frequency (Hz)');
@@ -36,11 +40,16 @@ f = Fs*(0:(L/2))/L;
 P2 = abs(fftSignal/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
+subplot(2,2,3);
 plot(f,P1);
 title('Single-Sided Amplitude Spectrum of X(t)')
 xlabel('Frequency (Hz)')
 ylabel('magnitude')
 
 %Plot spectrum in dB
-ResultDB = 20 * log10(P1 / (L/2));
+ResultDB = mag2db(P1); % 20*log10(P1);
+subplot(2,2,4);
 plot(f, ResultDB);
+title('Single-Sided Amplitude Spectrum of X(t) in DB');
+xlabel('Frequency (Hz)');
+ylabel('magnitude (DB)');
