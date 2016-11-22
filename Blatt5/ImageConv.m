3c57e4ea16eb73fb578a5944e3e0b0d10ada1631
Julius Hackel <julius.hackel@gmx.de>
2016-11-22 11:10:23 +0100

    Bugfixes

--- Blatt5/ImageConv.m
+++ Blatt5/ImageConv.m
@@ -1,5 +1,5 @@
 function [ outputImg ] = ImageConv( image, G )
-
+image = double(image)/255;
 i = imfilter(image,G);
 
 outputImg = i;
--- Blatt5/rankoperationIm.m
+++ Blatt5/rankoperationIm.m
@@ -1,4 +1,5 @@
 function [ outputImg ] = rankoperationIm( rankOp, image )
+image = double(image)/255;
 se = strel('rectangle',[7 7]);
 outputImg = feval(rankOp,image,se);
 end
