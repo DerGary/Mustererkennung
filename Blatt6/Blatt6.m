8331825267698bdb78c69cb5329237e4dd008244
MBulli <markusbullmann@gmail.com>
2016-11-29 10:25:17 +0100

    Stuff

--- Blatt6/Blatt6.m
+++ Blatt6/Blatt6.m
@@ -1,4 +1,4 @@
-%---Aufgabe1---%
+%% ---Aufgabe1--- %%
 A = [6 9; 9 7];
 %A = [1 2;2 0;0 1;1 1];
 %A = [1 2 1;2 0 1;0 1 0;1 1 3];
@@ -19,20 +19,36 @@ U = A*V/S;
 A
 AErg = U*S*V'
 
-%---Aufgabe2b/c---%
+%% ---Aufgabe2b/c--- %%
 %A = [3 4;-2 3;5 -7];   %b
-A = [1 3;5 2;4 -3];     %c
-b = [31; -7.5; -15.2];
+A = [ 1  3
+      5  2
+      4 -3 ];     %c
+b = [  31
+      -7.5
+      -15.2 ];
 
 [U,S,V]=svd(A,0);
 
-xSVD= V*((U'*b)./diag(S)) % x = A^+ * b mit A^+ = V Sigma^+ U'
+xSVD= V/S*U'*b % x = A^+ * b mit A^+ = V Sigma^+ U'
 xNormal= A\b
 
 K = cond(A)     % die numerische Lösung variiert um die k letzten Stellen von der exakten Lösung
                 % k = sigmaMax(A)/sigmaMin(A)
 
+%% 
+A = [
+    1  3
+    5  2
+    4 -3
+];
 
+b = [
+    3.9
+    7.3
+    1
+];
 
+[U,S,V]=svd(A,0);
 
-
+xSVD= V/S*U'*b
